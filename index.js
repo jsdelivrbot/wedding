8156b3aabfa679e27d04f1dd987fdde9f2da75f3
Chris Tung <chptung@gmail.com>
2017-12-23 22:39:09 -0500

    added meta tags

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/our-story', (req, res) => res.render('pages/our-story'))
-  .get('/rsvp', (req, res) => res.render('pages/rsvp'))
-  .get('/accommodations', (req, res) => res.render('pages/accommodations'))
-  .get('/our-photos', (req, res) => res.render('pages/photos'))
-  .get('*', (req, res) => res.render('pages/error'))
+  .get('/', (req, res) => res.render('pages/index', {
+    page: req.url }))
+  .get('/our-story', (req, res) => res.render('pages/our-story', {
+    page: req.url }))
+  .get('/rsvp', (req, res) => res.render('pages/rsvp', {
+    page: req.url }))
+  .get('/accommodations', (req, res) => res.render('pages/accommodations', {
+    page: req.url }))
+  .get('/our-photos', (req, res) => res.render('pages/photos', {
+    page: req.url }))
+  .get('*', (req, res) => res.render('pages/error', {
+    page: req.url }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
