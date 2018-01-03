2c9192e95fbaae85dcdaf8609205e7d832136e35
Chris Tung <chptung@gmail.com>
2018-01-02 16:40:50 -0800

    added photos revamp. working loop but images are wonky

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+function getImages() {
+  var fs = require('fs');
+  var sourceUrl = process.argv[1].split("/")
+
+  var imagePath = '/' + sourceUrl[1] + '/' + sourceUrl[2] + '/' + sourceUrl[3] + '/public/images'
+  var items = fs.readdirSync(imagePath);
+
+  return items;
+
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -14,8 +25,13 @@ express()
     page: req.url }))
   .get('/accommodations', (req, res) => res.render('pages/accommodations', {
     page: req.url }))
-  .get('/our-photos', (req, res) => res.render('pages/photos', {
-    page: req.url }))
+  /* .get('/our-photos', (req, res) => res.render('pages/photos', {
+    page: req.url })) */
+  .get('/our-photos', (req, res) => {
+    var items = getImages();
+    console.log(items);
+    res.render('pages/photos', {page: req.url, imagesArray: items})
+  })
   .get('*', (req, res) => res.render('pages/error', {
     page: req.url }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
